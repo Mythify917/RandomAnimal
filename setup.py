29b5cd8d1672283a8f27c1978889a0527256aadb
The Myth <68088818+themythlegbotdev@users.noreply.github.com>
2020-11-09 10:21:08 -0800

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='RandomAnimal',
+      version='0.1',
+      description='Get random animal photos.',
+      url='https://github.com/Mythify917/RandomAnimal',
+      author='Mythify',
+      author_email='thenoobdude21.12@gmail.com',
+      license='MIT',
+      packages=['requests', 'RandomAnimal'],
+      zip_safe=False)
