942e35751828880db7d0a4f8b7498a0760020d8d
The Myth <68088818+themythlegbotdev@users.noreply.github.com>
2020-11-08 16:19:46 -0800

    Update RandomAnimal.py

--- RandomAnimal.py
+++ RandomAnimal.py
@@ -2,12 +2,18 @@ import requests
 
 
 def RandomDog():
+    """
+    Uses the API to get a random image of a dog.
+    """
     url = f'https://random.dog/woof.json'
     res = requests.get(url).json()
     return res['url']
 
 
 def RandomCat():
+    """
+    Uses the API to get a random image of a cat.
+    """
     url = f"http://aws.random.cat/meow"
     res = requests.get(url).json()
     return res['file']
