9aef47238e55f76adb4d216e9a69e23007bf7ab4
The Myth <68088818+themythlegbotdev@users.noreply.github.com>
2020-11-09 10:12:57 -0800

    removed the ()

--- RandomAnimal.py
+++ RandomAnimal.py
@@ -1,28 +1,55 @@
 import requests
 
 
-def dog():
-    """
-    Uses the API to get a random image of a dog.
-    """
+def dog_api():
     url = f'https://random.dog/woof.json'
     res = requests.get(url).json()
     return res['url']
 
 
-def cat():
-    """
-    Uses the API to get a random image of a cat.
-    """
+dog = dog_api()
+
+
+def cat_api():
     url = f"http://aws.random.cat/meow"
     res = requests.get(url).json()
     return res['file']
 
 
-def fox():
-    """
-    Uses the API to get a random image of a fox.
-    """
+cat = cat_api()
+
+
+def fox_api():
     url = f"https://randomfox.ca/floof/"
     res = requests.get(url).json()
     return res['image']
+
+
+fox = fox_api()
+
+
+def panda_api():
+    url = f"https://some-random-api.ml/img/panda"
+    res = requests.get(url).json()
+    return res['link']
+
+
+panda = panda_api()
+
+
+def bird_api():
+    url = "https://some-random-api.ml/img/birb"
+    res = requests.get(url).json()
+    return res['link']
+
+
+bird = bird_api()
+
+
+def koala_api():
+    url = "https://some-random-api.ml/img/koala"
+    res = requests.get(url).json()
+    return res['link']
+
+
+koala = koala_api()
